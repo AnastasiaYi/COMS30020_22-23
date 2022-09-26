1aa4d10da3e074ea06d4303ad2d91afdcdf1fcdb
AnastasiaYi16 <lm20690@bristol.ac.uk>
2022-09-26 15:14:20 +0100

    first commit

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -11,7 +11,7 @@ void draw(DrawingWindow &window) {
 	window.clearPixels();
 	for (size_t y = 0; y < window.height; y++) {
 		for (size_t x = 0; x < window.width; x++) {
-			float red = rand() % 256;
+			float red = rand()%256;
 			float green = 0.0;
 			float blue = 0.0;
 			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
